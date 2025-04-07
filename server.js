83041d78483d25bb09e82aab95c3e4be28892e87
goutami <you@example.com>
2025-04-07 12:51:42 +0530

    intial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, AWS ECS CI/CD Pipeline!");
+    res.send("Hello, AWS ECS CI/CD Pipeline");
 });
 
 app.listen(port, () => {
